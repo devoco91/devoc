a9fb6204f0932cdbffcddadbfa2f7a017e2ba254
Damilola <temsdams1@gmail.com>
2023-06-18 05:56:40 -0700

    html added

---
+++ js/script.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html><html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my web</title>
+    <link rel="stylesheet" href="./ella.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <footer>
+	<div class="footer">
+	<div >
+	<h3>ENGRDAVE</h3>
+	<hr>
+	<p>Lorem ipsum dolor sit amet, qui facilis epicuri at. Vis quando timeam ex, alia natum ut vix. At alii eros commune ius, discere nominati usu in, te vis dictas dolorum facilisi. Ei his semper laoreet suscipiantur.</p>
+</div>
+
+	<div >
+		<h3>ABOUT US</h3>
+		<hr>
+		<p>Etiam admodum qualisque cu nec, autem impedit repudiandae usu ut. Ea platonem abhorreant nam, purto iudico vix et, eu est alia dicant perpetua.</p>
+	</div>
+
+		<div >
+			<h3>CONTACT INFO</h3>
+			<hr>
+			<ul>
+			<li>
+			<p>
+			<i class="fa fa-home"></i>
+			78, old Olowoira, road, Ojodu Berger, Lagos.
+			</p>
+
+			<p>
+			<i class="fa fa-phone"></i>
+			0702 571 3326
+		</p>
+
+			<p>
+			<i class="fa fa-envelope-o"></i>
+			support@lasop.net
+			</p>
+			</li>
+			</ul>
+	</div>
+	</div>
+	</footer>
+    
+    <div class="dammy" >
+		<p>© 2022 <span>engrDavid</span> All Rights Reserved</p>
+	</div>
+
+</body>
+</html>
